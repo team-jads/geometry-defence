c8d4939cc7e12d6be66469eba7a93357db6e8938
S Basil <SBasil20@users.noreply.github.com>
2019-06-23 19:06:59 -0400

    find-player-ui

--- GeometryDefense/app/src/main/java/com/jads/geometrydefense/MainActivity.java
+++ GeometryDefense/app/src/main/java/com/jads/geometrydefense/MainActivity.java
@@ -8,14 +8,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button startGameButton;
+        Button findPlayerButton;
+
 
         startGameButton = findViewById(R.id.start_game);
         startGameButton.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(gamePage);
             }
         });
+
+        findPlayerButton = findViewById(R.id.find_player);
+        findPlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent findPlayerPage = new Intent(MainActivity.this, FindPlayerActivity.class);
+                startActivity(findPlayerPage);
+            }
+        });
+
     }
 }
