0ddbc6532e78cb44f3c214a3201fdd140da9d253
SBasil20 <shruti.basil@gmail.com>
2019-07-12 21:19:13 -0400

    Added record gameplay video

--- GeometryDefense/app/src/main/java/com/jads/geometrydefense/GamePageActivity.java
+++ GeometryDefense/app/src/main/java/com/jads/geometrydefense/GamePageActivity.java
@@ -6,6 +6,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.KeyEvent;
+
 import com.jads.geometrydefense.entities.TurretLand;
 
 public class GamePageActivity extends AppCompatActivity {
@@ -14,6 +33,23 @@ public class GamePageActivity extends AppCompatActivity {
     private GameBoardCanvas canvas;
     private ImageView tower;
     private boolean gameStarted = false;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_CODE = 1000;
+    private static final int PERMISSION_REQ_ID_RECORD_AUDIO = 22;
+    private static final int PERMISSION_REQ_ID_WRITE_EXTERNAL_STORAGE = PERMISSION_REQ_ID_RECORD_AUDIO + 1;
+
+    private boolean isPermissible = false;
+    private int mScreenWidth;
+    private int mScreenHeight;
+    private int mScreenDensity;
+
+    private boolean isVideoSd;
+    private boolean isAudio;
+
+    private Button record;
+    public boolean recording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +80,31 @@ public class GamePageActivity extends AppCompatActivity {
             }
         });
 
-    }
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO) && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, PERMISSION_REQ_ID_WRITE_EXTERNAL_STORAGE)) {
+            isPermissible = true;
+        }
+        Log.d(TAG, "onCreate: isPermissible = " + isPermissible);
+        isVideoSd = true;
+        getScreenBaseInfo();
 
+
+        record = findViewById(R.id.record);
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!recording) {
+                    record.setText("Stop");
+                    if (isPermissible) {
+                        startScreenRecording();
+                    }
+                } else {
+                    record.setText("Record");
+                    stopScreenRecording();
+                }
+                recording = !recording;
+            }
+        });
+    }
 
     public void focusOn() {
         tower.setVisibility(View.VISIBLE);
@@ -55,4 +114,113 @@ public class GamePageActivity extends AppCompatActivity {
         tower.setVisibility(View.GONE);
     }
 
+    public boolean checkSelfPermission(String permission, int requestCode) {
+        Log.d(TAG, "checkSelfPermission " + permission + " " + requestCode);
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult " + grantResults[0] + " " + requestCode);
+        switch (requestCode) {
+            case PERMISSION_REQ_ID_RECORD_AUDIO:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, PERMISSION_REQ_ID_WRITE_EXTERNAL_STORAGE);
+                } else {
+                    isPermissible = false;
+                    showLongToast("No permission for " + Manifest.permission.RECORD_AUDIO);
+                    finish();
+                }
+                break;
+            case PERMISSION_REQ_ID_WRITE_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: isPermissible = " + isPermissible);
+                    isPermissible = true;
+                } else {
+                    isPermissible = false;
+                    showLongToast("No permission for " + Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public final void showLongToast(final String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void getScreenBaseInfo() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenWidth = metrics.widthPixels;
+        mScreenHeight = metrics.heightPixels;
+        mScreenDensity = metrics.densityDpi;
+    }
+
+    private void startScreenRecording() {
+        MediaProjectionManager mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Intent permissionIntent = mediaProjectionManager != null ? mediaProjectionManager.createScreenCaptureIntent() : null;
+        startActivityForResult(permissionIntent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Intent service = new Intent(this, ScreenRecordService.class);
+                service.putExtra("code", resultCode);
+                service.putExtra("data", data);
+                service.putExtra("audio", isAudio);
+                service.putExtra("width", mScreenWidth);
+                service.putExtra("height", mScreenHeight);
+                service.putExtra("density", mScreenDensity);
+                service.putExtra("quality", isVideoSd);
+                startService(service);
+                Log.i(TAG, "Start screen recording");
+            } else {
+                Toast.makeText(this, "user cancelled", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "User cancelled");
+            }
+        }
+    }
+
+    private void stopScreenRecording() {
+        Intent service = new Intent(this, ScreenRecordService.class);
+        stopService(service);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            moveTaskToBack(true);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
+
