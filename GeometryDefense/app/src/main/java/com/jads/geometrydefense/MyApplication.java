cc6071334838a01dda5499aca8493555d5a277d7
Daniel Whitney <tiedye1@hotmail.com>
2019-07-22 10:24:09 -0400

    remove references to uncommited things

--- GeometryDefense/app/src/main/java/com/jads/geometrydefense/MyApplication.java
+++ GeometryDefense/app/src/main/java/com/jads/geometrydefense/MyApplication.java
@@ -16,32 +16,10 @@ public class MyApplication extends Application {
         return mContext;
     }
 
-
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         mContext = getApplicationContext();
-
-        loadGameData();
-    }
-
-    public void loadGameData() {
-        GameConfig gameConfig = GameConfig.getInstance();
-
-
-
-        gameConfig.gameData = new StaticGameData()
-                .addCreepType(new CreepType(R.drawable.ic_box1, 2, 1, 1.5f))
-                .addCreepType(new CreepType(R.drawable.ic_box2, 5, 5, 1.5f))
-                .addCreepType(new CreepType(R.drawable.ic_box3, 13, 10, 1.5f))
-                .addCreepType(new CreepType(R.drawable.ic_box4, 34, 20, 1.5f))
-                .addCreepType(new CreepType(R.drawable.ic_box5, 89, 60, 1.5f))
-                .addCreepType(new CreepType(R.drawable.ic_circ1, 3, 1, 1))
-                .addCreepType(new CreepType(R.drawable.ic_circ2, 8, 5, 1))
-                .addCreepType(new CreepType(R.drawable.ic_circ3, 21, 10, 1))
-                .addCreepType(new CreepType(R.drawable.ic_circ4, 55, 20, 1))
-                .addCreepType(new CreepType(R.drawable.ic_circ5, 144, 60, 1))
-        ;
     }
 }
